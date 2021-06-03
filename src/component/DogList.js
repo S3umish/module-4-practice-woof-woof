0569e3a145ef3d0dcdea9aceeac4b3edfbcd0881
sunitee <suniteebajpai2@gmail.com>
2021-06-03 11:16:56 -0400

    commit tree f7a83183dd8ce679c736eed100dd17c2b40ace8f

---
+++ src/component/DogList.js
@@ -0,0 +1,18 @@
+import React from "react"
+import DogItem from './DogItem'
+
+
+const DogList = (props) => {
+    const dogsToRender = props.dogs.map( dog => {
+        return <DogItem key={dog.id} dog={dog} handleClick={props.handleClick}/>
+    })
+    
+    return (
+        <div id="dog-list">
+            {dogsToRender}
+        </div>
+    )
+        
+}
+
+export default DogList
